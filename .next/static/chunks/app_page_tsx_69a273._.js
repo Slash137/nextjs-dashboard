d329a277225e5a57552fea310fb8d93ba75be4a0
Cayetano (MacOS) <cayetanoslash@gmail.com>
2025-01-31 23:35:55 +0100

    First commit

---
+++ .next/static/chunks/app_page_tsx_69a273._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_69a273._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_69a273._.js",
+  "chunks": [
+    "static/chunks/[next]_internal_font_google_323c5c._.css",
+    "static/chunks/adab6_next_dist_d67bfc._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js"
+  ],
+  "source": "dynamic"
+});
